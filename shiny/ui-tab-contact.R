c191be28be8e8b3e6c75be7693f7082ec62575b1
Dean Attali <daattali@gmail.com>
2017-02-04 12:00:42 -0500

    change `p` to `tags$p` beacuase of clashes with pander package

--- shiny/ui-tab-contact.R
+++ shiny/ui-tab-contact.R
@@ -1,11 +1,11 @@
 tagList(
-  p("The Work Environment Survey (WES) was in the field from October 6, 2015 through October 30, 2015."),
-  p("Do you have a question about WES? You have come to the right page."),
-  p("Our philosophy about questions is simple:"),
+  tags$p("The Work Environment Survey (WES) was in the field from October 6, 2015 through October 30, 2015."),
+  tags$p("Do you have a question about WES? You have come to the right page."),
+  tags$p("Our philosophy about questions is simple:"),
   tags$ul(
     tags$li("All questions are good questions."),
     tags$li("There are probably many people with the same question."),
     tags$li("The only bad question is the one that doesn't get asked.")
   ),
-  p("Over the years we have been asked many questions about the BC Public Service Work Environment Survey (WES). We have compiled a list of the most common questions asked by employees, managers, executives, human resource professionals, statisticians and other interested parties. These questions and our answers are listed below. See if you can find the answer to your question here. If your question is not answered, email us at", tags$a(href="mailto:Work.Environment.Survey@gov.bc.ca", "Work.Environment.Survey@gov.bc.ca"))
+  tags$p("Over the years we have been asked many questions about the BC Public Service Work Environment Survey (WES). We have compiled a list of the most common questions asked by employees, managers, executives, human resource professionals, statisticians and other interested parties. These questions and our answers are listed below. See if you can find the answer to your question here. If your question is not answered, email us at", tags$a(href="mailto:Work.Environment.Survey@gov.bc.ca", "Work.Environment.Survey@gov.bc.ca"))
 )
